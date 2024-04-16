3fd5698d9c7e6562adcf42af951c8c181ccf997c
Gustavo Alarcón <galarcon@meraki-s.com>
2024-04-16 17:57:06 -0500

    First commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,68 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import {
+  connectFunctionsEmulator,
+  getFunctions,
+  provideFunctions,
+} from '@angular/fire/functions';
+import {
+  connectStorageEmulator,
+  getStorage,
+  provideStorage,
+} from '@angular/fire/storage';
+import { environment } from '../environment/dev.environment';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp())),
+    importProvidersFrom(
+      provideAuth(() => {
+        const auth = getAuth();
+        if (environment.useEmulators) {
+          connectAuthEmulator(auth, 'http://localhost:9099', {
+            disableWarnings: true,
+          });
+        }
+        return auth;
+      })
+    ),
+    importProvidersFrom(
+      provideFirestore(() => {
+        const firestore = getFirestore();
+        if (environment.useEmulators) {
+          connectFirestoreEmulator(firestore, 'localhost', 8080);
+        }
+        return firestore;
+      })
+    ),
+    importProvidersFrom(
+      provideFunctions(() => {
+        const functions = getFunctions();
+        if (environment.useEmulators) {
+          connectFunctionsEmulator(functions, 'localhost', 5001);
+        }
+        return functions;
+      })
+    ),
+    importProvidersFrom(
+      provideStorage(() => {
+        const storage = getStorage();
+        if (environment.useEmulators) {
+          connectStorageEmulator(storage, 'localhost', 9199);
+        }
+        return storage;
+      })
+    ), provideAnimationsAsync(),
+  ],
 };
---
+++ src/environment/dev.environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebaseConfig: {
+    projectId: 'truekit-staging',
+    appId: '1:829574262941:web:378c88273c05a02d366474',
+    storageBucket: 'truekit-staging.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCULcCD-DzxVg0rYiUcRPCIn_PUzwP0fNU',
+    authDomain: 'truekit-staging.firebaseapp.com',
+    messagingSenderId: '829574262941',
+    measurementId: 'G-LHDYL41FR6',
+  },
+  useEmulators: true,
+};
