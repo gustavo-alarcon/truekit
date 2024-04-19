d0b34bfab55b5c0fc6eeb772e63d5c4b70cab3e5
Gustavo Alarcon <galarcon@meraki-s.com>
2024-04-19 18:43:57 -0500

    :sparkles: feature category card converted to swiper slider

--- src/app/shared/components/category-card/category-card.component.ts
+++ src/app/shared/components/category-card/category-card.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-category-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CategoryCardComponent {
-  @Input({ required: true }) title = '';
-  @Input({ required: true }) image: string = '';
+  @Input() title = '';
+  @Input() image: string = '';
+  @Input({ required: true }) categories: any[] = [];
   @Input() description = '';
 }
